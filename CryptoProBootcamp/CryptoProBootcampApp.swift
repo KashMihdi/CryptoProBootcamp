ebdd43ee10b526649d24f0e0a28b0f2ae72a1fc5
KashMihdi <127855730+KashMihdi@users.noreply.github.com>
2023-07-14 22:43:25 +0300

    Initial Commit

---
+++ CryptoProBootcamp/CryptoProBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoProBootcampApp.swift
+//  CryptoProBootcamp
+//
+//  Created by Kasharin Mikhail on 14.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoProBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
