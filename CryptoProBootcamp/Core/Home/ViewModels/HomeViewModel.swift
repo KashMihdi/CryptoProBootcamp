bdf8153cca34b0b357332f1ba431b3f05f06d47d
KashMihdi <127855730+KashMihdi@users.noreply.github.com>
2023-07-20 15:55:16 +0300

    #11 https://www.youtube.com/watch?v=D4zLYb-TgIU&list=PLwvDm4Vfkdphbc3bgy_LpLRQ9DDfFGcFu&index=12

--- CryptoProBootcamp/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoProBootcamp/Core/Home/ViewModels/HomeViewModel.swift
@@ -19,11 +19,34 @@ class HomeViewModel: ObservableObject {
     
     init() {
         addSubscribers()
-        print(allCoins.count)
     }
     
     func addSubscribers() {
+        /*
+         // старая подписка на получение данных из сети при помощи CoinDataService
         dataService.$allCoins
+            .sink { [weak self] returnedCoins in
+                self?.allCoins = returnedCoins
+            }
+            .store(in: &cancellable)
+        */
+        
+        // подписка для передачи массива из сети и фильтрация его при помощи поисковой строки
+        // замена функции сверху в комментарии
+        $searchText
+            .combineLatest(dataService.$allCoins)
+        // добавляем задержку для поисковой строки
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map { text, startingCoins -> [CoinModel] in
+                guard !text.isEmpty else { return startingCoins }
+                let lowercasedText = text.lowercased()
+                
+                return startingCoins.filter {
+                    $0.name.lowercased().contains(lowercasedText) ||
+                    $0.symbol.lowercased().contains(lowercasedText) ||
+                    $0.id.lowercased().contains(lowercasedText)
+                }
+            }
             .sink { [weak self] returnedCoins in
                 self?.allCoins = returnedCoins
             }
