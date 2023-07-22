1a8187f052b83fb16ffa065973a740d9b55e496e
KashMihdi <127855730+KashMihdi@users.noreply.github.com>
2023-07-22 09:00:29 +0300

    #13 https://www.youtube.com/watch?v=bLwUAe00jp8&list=PLwvDm4Vfkdphbc3bgy_LpLRQ9DDfFGcFu&index=14

--- CryptoProBootcamp/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoProBootcamp/Core/Home/ViewModels/HomeViewModel.swift
@@ -13,14 +13,10 @@ class HomeViewModel: ObservableObject {
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
     @Published var searchText = ""
-    @Published var statistics: [StatisticModel] = [
-        StatisticModel(title: "Title", value: "Value", percentageChange: 1),
-        StatisticModel(title: "Title", value: "Value"),
-        StatisticModel(title: "Title", value: "Value"),
-        StatisticModel(title: "Title", value: "Value", percentageChange: -7)
-    ]
+    @Published var statistics: [StatisticModel] = []
     
-    private let dataService = CoinDataService()
+    private let coinDataService = CoinDataService()
+    private let marketDataService = MarketDataService()
     private var cancellable = Set<AnyCancellable>()
     
     init() {
@@ -40,7 +36,7 @@ class HomeViewModel: ObservableObject {
         // подписка для передачи массива из сети и фильтрация его при помощи поисковой строки
         // замена функции сверху в комментарии
         $searchText
-            .combineLatest(dataService.$allCoins)
+            .combineLatest(coinDataService.$allCoins)
         // добавляем задержку для поисковой строки
             .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
             .map { text, startingCoins -> [CoinModel] in
@@ -57,5 +53,33 @@ class HomeViewModel: ObservableObject {
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancellable)
+        
+        /*
+        подписка для заполнения массива statistics, который отображает краткую инормацию вверху главной страницы по капитализации
+         для нее создан отдельный файл для загрузки данных MarketDataService
+        */
+        marketDataService.$marketData
+            .map( mapGlobalMarketData )
+            .sink { [weak self] stats in
+                self?.statistics = stats
+            }
+            .store(in: &cancellable)
+            
+    }
+    
+    // вспомогательная функция
+    private func mapGlobalMarketData(data: MarketDataModel?) -> [StatisticModel] {
+        var stats: [StatisticModel] = []
+        
+        guard let data else { return stats }
+        
+        let marketCap = StatisticModel(title: "Market Cap", value: data.marketCap, percentageChange: data.marketCapChangePercentage24HUsd)
+        let volume = StatisticModel(title: "24H Volume", value: data.volime)
+        let btcDominance = StatisticModel(title: "BTC Dominance", value: data.btcDominance)
+        let portfolio = StatisticModel(title: "Portfolio", value: "$0.00", percentageChange: 0)
+        
+        stats.append(contentsOf: [marketCap, volume, btcDominance, portfolio])
+        
+        return stats
     }
 }
---
+++ CryptoProBootcamp/Services/MarketDataService.swift
@@ -0,0 +1,33 @@
+//
+//  MarketDataService.swift
+//  CryptoProBootcamp
+//
+//  Created by Kasharin Mikhail on 21.07.2023.
+//
+
+import Foundation
+import Combine
+
+
+/*
+класс по загрузке статистических данных из интеренета
+*/
+class MarketDataService {
+    @Published var marketData: MarketDataModel? = nil
+    var marketDataSubscriber: AnyCancellable?
+    
+    init() {
+        getData()
+    }
+    
+    private func getData() {
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/global") else { return }
+        
+        marketDataSubscriber = NetworkingManager.download(url: url)
+            .decode(type: GlobalData.self, decoder: JSONDecoder())
+            .sink(receiveCompletion: NetworkingManager.handleCompletion) { [weak self] returnedGlobalData in
+                self?.marketData = returnedGlobalData.data
+                self?.marketDataSubscriber?.cancel()
+            }
+    }
+}
